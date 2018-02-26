a434637a275b8890406e8669a6331756830d1f7a
Tamas <deaktamas93@gmail.com>
2018-02-26 16:49:58 +0100

    Initial Create

---
+++ Arteg/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Arteg.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AsianVital()
+        {
+            return View();
+        }
+
+        public IActionResult Atreg()
+        {
+            return View();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Atreg-second
+# atreg
+atreg website</br>
+a ceg neve <strong>ATREG...</strong> folyamat 'arteg'nek irom, ne zavarjon meg :D
