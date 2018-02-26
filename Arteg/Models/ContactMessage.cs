3609274bb3fe8ee9dd8610e14eafce99b69dcae0
Tamas <deaktamas93@gmail.com>
2018-02-26 23:40:04 +0100

    feat(Models): add ContactMessage model for POST methods

---
+++ Arteg/Models/ContactMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arteg.Models
+{
+    public class ContactMessage
+    {
+        public string EmailAddress { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public DateTime SentDateTime { get; set; }
+    }
+}
