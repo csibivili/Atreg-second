04b89897b920d5c49af425ca851b93933952d867
Tamas <deaktamas93@gmail.com>
2018-02-27 11:49:30 +0100

    feat(Services): add mail sending service and its DI into controller

---
+++ Arteg/Services/ContactService.cs
@@ -0,0 +1,66 @@
+﻿using Arteg.Models;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Arteg.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly string _userName = "atreg.info@gmail.com";
+        private readonly string _password = "atreg2k18";
+
+        public async Task SendMail(ContactMessage message)
+        {
+            try
+            {
+                #region html message composition
+                StringBuilder mailBody = new StringBuilder()
+                  .Append
+                  (
+                    $"<html><head></head><body>" +
+                    $"<span style =\"font-weight:bold\">" +
+                    $"From:</span><br />" +
+                    $"<span>{ message.Name}</span><br />" +
+                    $"<span>{ message.EmailAddress}</span><br /><br />" +
+                    $"<span style =\"font-weight:bold\">" +
+                    $"Subject:</span><br />" +
+                    $"<span>{ message.Subject}</span><br /><br />" +
+                    $"<span style =\"font-weight:bold\">" +
+                    $"Text:</span><br />" +
+                    $"<span>{ message.Text}</span></body></html>"
+                  );
+                #endregion
+
+                using (var mail = new MailMessage(message.EmailAddress, "csibivili@gmail.com"))
+                {
+                    mail.To.Add(new MailAddress("csibivili@gmail.com"));
+                    mail.From = new MailAddress(message.EmailAddress);
+                    mail.Subject = $"Message from {message.EmailAddress} on the Atreg website";
+                    mail.IsBodyHtml = true;
+                    mail.Body = mailBody.ToString();
+
+                    #region smtp configuration
+                    using (var client = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        client.EnableSsl = true;
+                        client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        client.UseDefaultCredentials = false;
+                        client.Credentials = new NetworkCredential(_userName, _password);
+                        client.Timeout = 20000;
+
+                        await client.SendMailAsync(mail);
+                    }
+                    #endregion
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+        }
+    }
+}
---
+++ Arteg/Services/IContactService.cs
@@ -0,0 +1,13 @@
+﻿using Arteg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arteg.Services
+{
+    public interface IContactService
+    {
+        Task SendMail(ContactMessage message);
+    }
+}
